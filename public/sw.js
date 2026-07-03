6dafb29997fbe295d3f846c4cda07423a9a0fc36
Alok <aloksk1234@gmail.com>
2026-07-03 09:33:43 +0530

    Create solid-background pwa-logo.png and update manifest to resolve broken mobile icons

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,9 @@
-const CACHE_NAME = 'cinememe-cache-v1';
+const CACHE_NAME = 'cinememe-cache-v2';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/logo.png',
+  '/pwa-logo.png',
   '/og-image.png'
 ];
 
